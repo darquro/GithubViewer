fab59edfd3fe6321bdc54cba2a73a3f76f8214c7
darquro <yuki.kuro96@gmail.com>
2021-11-07 19:09:46 +0900

    fix directory

--- FeatureModules/Search/Package.swift
+++ FeatureModules/Search/Package.swift
@@ -18,8 +18,8 @@ let package = Package(
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
         .package(path: "WebContent"),
-        .package(path: "../Modules/ViewComponents"),
-        .package(path: "../Modules/Repositories"),
+        .package(path: "../CoreModules/ViewComponents"),
+        .package(path: "../CoreModules/Repositories"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
